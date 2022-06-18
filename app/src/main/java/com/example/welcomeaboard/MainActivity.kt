98080d72b699cb45930eac621ae72b5acf486962
halilbalci <halilbalci878@outlook.com>
2022-06-19 02:45:35 +0300

    calculate tip

--- app/src/main/java/com/example/welcomeaboard/MainActivity.kt
+++ app/src/main/java/com/example/welcomeaboard/MainActivity.kt
@@ -4,15 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import com.example.welcomeaboard.databinding.ActivityMainBinding
+import com.example.welcomeaboard.databinding.TipCalculatorBinding
 import com.example.welcomeaboard.dice_example.Dice
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: TipCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.barbut_playground)
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { playBarbut() }
+        binding = TipCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculate.setOnClickListener{calculateTip()}
+
+        //setContentView(R.layout.barbut_playground)
+        //val rollButton: Button = findViewById(R.id.button)
+        //rollButton.setOnClickListener { playBarbut() }
     }
+    private fun calculateTip(){
+        val costString = binding.textInputTip.text.toString()
+        val costDouble = costString.toDoubleOrNull()
+        if (costDouble == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercent = when(binding.radioGroup.checkedRadioButtonId){
+            R.id.percent20-> 0.20
+            R.id.percent18-> 0.18
+            else-> 0.15
+        }
+        var tip = tipPercent*costDouble
+        if(binding.switchCompat.isChecked) tip = kotlin.math.ceil(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, NumberFormat.getCurrencyInstance().format(tip))
+    }
+
     fun playBarbut(){
         val diceImage: ImageView = findViewById(R.id.imageView2)
         val diceImage2: ImageView = findViewById(R.id.imageView3)
